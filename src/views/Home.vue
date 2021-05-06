8758b2ef10f9f315c1f0a966e5e9f871050368f0
EDAIG <edaigkhalid@gmail.com>
2021-05-06 21:05:23 +0000

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Vue 3 Start Dashboard ui using tailwind css and vue3 ...
+    <br>
+    Welcome to build Amazing app together...
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
